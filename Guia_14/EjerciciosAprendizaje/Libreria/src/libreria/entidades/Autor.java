c0773527a82a71585a758115f62e49ef2e679f35
Alejandro Fernandez <alejandrofernandez21.af@gmail.com>
2022-10-05 19:43:00 -0300

    Otra vez

--- Guia_14/EjerciciosAprendizaje/Libreria/src/libreria/entidades/Autor.java
+++ Guia_14/EjerciciosAprendizaje/Libreria/src/libreria/entidades/Autor.java
@@ -1,7 +1,16 @@
 package libreria.entidades;
 
-public class Autor {
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Autor implements Serializable {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
     private Boolean alta;
--- Guia_14/EjerciciosAprendizaje/Libreria/src/libreria/entidades/Editorial.java
+++ Guia_14/EjerciciosAprendizaje/Libreria/src/libreria/entidades/Editorial.java
@@ -1,7 +1,16 @@
 package libreria.entidades;
 
-public class Editorial {
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Editorial implements Serializable {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
     private Boolean alta;
